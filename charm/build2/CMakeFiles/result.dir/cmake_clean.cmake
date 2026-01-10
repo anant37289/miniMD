9fa7ad7992cb2f7cf2ea22be26ff4b1577a3d834
shourya goel <shouryagoel10000@gmail.com>
2026-01-10 10:01:20 -0600

    remove build

--- charm/build2/CMakeFiles/result.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/result"
-  "miniMD"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/result.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
