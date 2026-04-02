58c6166beb1193e528d600150acd077b74a4e6b2
Shourya Goel <shouryagoel10000@gmail.com>
2026-04-02 04:35:01 -0500

    cmake files

--- charm/config.cmake
+++ charm/config.cmake
@@ -1,5 +1,5 @@
 set(KOKKOS_DIR "/u/ajain18/belobench/LibCharmtyles/kokkos/install")
-set(CHARM_DIR "/u/ajain18/charm/mpi-linux-x86_64-smp-cuda/")
+set(CHARM_DIR "/u/ajain18/oldCharm/mpi-linux-x86_64-smp-cuda/")
 set(CHARMC "${CHARM_DIR}/bin/charmc")
 set(CUDA_DIR "/opt/nvidia/hpc_sdk/Linux_x86_64/25.3/cuda/12.8")
 set(BASE_DIR "/u/ajain18/miniMD/charm")
@@ -7,4 +7,4 @@ set(BASE_DIR "/u/ajain18/miniMD/charm")
 set(Charm_ENABLE_GPU ON)
 
 set(GPU_OPTS "-O3 -std=c++20 -extended-lambda -Wext-lambda-captures-this")
-set(GPU_LINK_OPTS "-O3 -std=c++20 -lcudart -lcuda -L${KOKKOS_DIR}/lib64 -lkokkoscore -lkokkoscontainers -lkokkosalgorithms -lkokkossimd")
+set(GPU_LINK_OPTS "-O3 -std=c++20 -lcudart -lcuda -L${KOKKOS_DIR}/lib64 -lkokkoscore -lkokkoscontainers -lkokkosalgorithms -lkokkossimd -module EveryLB")
